d4f3603855386233d2bdd666f18937bb0e5270bc
gongeek <gongweigm@gmail.com>
2015-05-21 11:52:32 +0800

    real start

--- js/mapInit.js
+++ js/mapInit.js
@@ -5,34 +5,36 @@ require([
 	'dijit/layout/BorderContainer', 'dijit/layout/ContentPane', 'dojo/domReady!'
 ], function (Map, WMSLayer, WMSLayerInfo, Extent, array, dom, domConst, parser,SpatialReference) {
 	parser.parse();
-	var defaultExtent = new Extent(-20037508, -20037508, 20037508, 20037508.34, {wkid: 900913});
+	var defaultExtent = new Extent(-4227935.2725877, -7877419.4028669,12864532.589711,6964891.6517091, {wkid: 900913});
 	map = new Map('map', {
 		extend: defaultExtent,
 		basemap: 'streets',
-		center: [-96, 37],
-		zoom: 4
+		autoResize:true,
+		zoom:2
 	});
 
 	var layer1 = new WMSLayerInfo({
 		name: 'cglc30_2000_0',
 		title: 'Rivers',
-		SpatialReference:new SpatialReference(900913)
+		spatialReferences:new SpatialReference(900913)
 	});
-	var layer2 = new WMSLayerInfo({
-		name: 'cglc30_2010_0',
-		title: 'Cities',
-		SpatialReference:new SpatialReference(900913)
-	});
+	//var layer2 = new WMSLayerInfo({
+	//	name: 'cglc30_2010_0',
+	//	title: 'Cities'
+	//});
 	var resourceInfo = {
 		extent:defaultExtent,
-		layerInfos: [layer1, layer2],
-		version: '1.1.1'
+		layerInfos: [layer1],
+		version: '1.1.1',
+		maxHeight:256,
+		maxWidth:256
 	};
 	var wmsLayer = new WMSLayer('http://218.244.250.80:8080/erdas-apollo/coverage/CGLC', {
 		resourceInfo: resourceInfo,
-		visibleLayers: ['cglc30_2000_0', 'cglc30_2010_0']
+		visibleLayers: ['cglc30_2000_0'],
+		extend:defaultExtent
 	});
-	debugger;
+	wmsLayer.spatialReferences[0] = 900913;
 	map.addLayers([wmsLayer]);
 })
 ;
